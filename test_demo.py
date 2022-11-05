7a92d5e3601f3c4bc629e7cd8c27d950238de7a7
”Natalia <n.i.tsyrelchuk@gmail.com>
2022-11-05 22:15:30 +0500

    added test

---
+++ test_demo.py
@@ -0,0 +1,27 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("https://www.saucedemo.com/")
+
+def test_title():
+    title_from_site = driver.title
+    assert title_from_site =='Swag Labs'
+
+def test_login_form():
+    driver.find_element(By.ID, 'user-name').send_keys('standard_user')
+    time.sleep(1)
+
+    password = driver.find_element(By.XPATH, "//input[@id='password']").send_keys('secret_sauce')
+    time.sleep(1)
+
+    button_login =driver.find_element(By.XPATH, "//input[@name='login-button']").click()
+    time.sleep(1)
+
+    assert driver.current_url == 'https://www.saucedemo.com/inventory.html', 'We reached another site!'
+
