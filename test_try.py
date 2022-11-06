32e011dbd1cc33e0f66249eb285558d5b8909214
”Natalia <n.i.tsyrelchuk@gmail.com>
2022-11-06 22:23:39 +0500

    black and flake8

--- test_try.py
+++ test_try.py
@@ -4,25 +4,27 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get("https://www.saucedemo.com/")
 
+
 def test_title():
     title_from_site = driver.title
-    assert title_from_site =='Swag Labs'
+    assert title_from_site == "Swag Labs"
+
 
 def test_login_form():
-    driver.find_element(By.ID, 'user-name').send_keys('standard_user')
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
     time.sleep(3)
 
-    password = driver.find_element(By.XPATH, "//input[@id='password']").send_keys('secret_sauce')
+    driver.find_element(By.XPATH, "//input[@id='password']"
+                        ).send_keys("secret_sauce")
     time.sleep(3)
 
-    button_login =driver.find_element(By.XPATH, "//input[@name='login-button']").click()
+    driver.find_element(By.XPATH, "//input[@name='login-button']").click()
     time.sleep(3)
 
-    assert driver.current_url == 'https://www.saucedemo.com/inventory.html', \
-        'We reached another site!'
-
+    assert (
+        driver.current_url == "https://www.saucedemo.com/inventory.html"
+    ), "We reached another site!"
