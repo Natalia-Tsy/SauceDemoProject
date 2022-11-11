e1918a912f46aad5d716c54133f8384a13a34e0f
Alexander Tomelo <qa.tomelo.an@gmail.com>
2022-11-11 15:07:32 +0300

    correct PEP8

--- test_successful_user_login_performance_glitch_user.py
+++ test_successful_user_login_performance_glitch_user.py
@@ -8,23 +8,29 @@ from selenium.webdriver.common.by import By
 
 def init_browser():
     """Функция создания драйвера Хром"""
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    return browser
+    browser1 = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    return browser1
 
 
-def test_login_user_performance_glitch_user(browser:webdriver.Chrome):
+def test_login_user_performance_glitch_user(browser2: webdriver.Chrome):
     """Тест проверки успешной авторизации пользователя performance_glitch_user"""
-    browser.get("https://www.saucedemo.com/")
-    browser.find_element(By.CSS_SELECTOR, "input[placeholder='Username']").send_keys('performance_glitch_user')
+    browser2.get("https://www.saucedemo.com/")
+    browser2.find_element(By.CSS_SELECTOR, "input[placeholder='Username']").send_keys(
+        "performance_glitch_user"
+    )
     time.sleep(2)
-    browser.find_element(By.CSS_SELECTOR,"input[placeholder='Password']").send_keys('secret_sauce')
+    browser2.find_element(By.CSS_SELECTOR, "input[placeholder='Password']").send_keys(
+        "secret_sauce"
+    )
     time.sleep(2)
-    browser.find_element(By.CSS_SELECTOR,"#login-button").click()
+    browser.find_element(By.CSS_SELECTOR, "#login-button").click()
     time.sleep(2)
     login_url = browser.current_url
-    assert "https://www.saucedemo.com/inventory.html" == login_url, "The current page does not correspond to the page the user should be on after successful registration"
+    assert (
+        "https://www.saucedemo.com/inventory.html" == login_url
+    ), "The current page does not correspond to the page the user should be on after successful registration"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     browser = init_browser()
     test_login_user_performance_glitch_user(browser)
