56d83bf63f9752349f05d42075b6214d9df384ca
Oleg K <114308068+OlegKomissarovV@users.noreply.github.com>
2022-11-16 06:00:09 +0300

    refactor again checkout_1_page

--- pages/checkout_1_page.py
+++ pages/checkout_1_page.py
@@ -42,17 +42,11 @@ class CheckoutPage_1(BasePage):
     # Заполняются данные, необходимые для получения товара
     def set_shipping_info(self, firstname, lastname, code):
         # Имя пользователя передается текстовому элементу на странице
-        self.browser.find_element(
-            *Checkout1PageLocators.INPUT_FIRSTNAME
-        ).send_keys(firstname)
+        self.browser.find_element(*Checkout1PageLocators.INPUT_FIRSTNAME).send_keys(firstname)
         # Пароль передается текстовому элементу на странице
-        self.browser.find_element(
-            *Checkout1PageLocators.INPUT_LASTNAME
-        ).send_keys(lastname)
+        self.browser.find_element(*Checkout1PageLocators.INPUT_LASTNAME).send_keys(lastname)
         # Код получения передается текстовому элементу на странице
-        self.browser.find_element(*Checkout1PageLocators.INPUT_CODE).send_keys(
-            code
-        )
+        self.browser.find_element(*Checkout1PageLocators.INPUT_CODE).send_keys(code)
 
     # Нажимается кнопка "LOGIN"
     def go_to_checkout_2_page(self):
