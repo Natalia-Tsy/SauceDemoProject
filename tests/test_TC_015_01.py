659cad29a945faf1834373413a660bbf57a76a3c
Alexander Tomelo <qa.tomelo.an@gmail.com>
2022-11-20 03:41:34 +0300

    correct test_TC_015_01.py file by black

--- tests/test_TC_015_01.py
+++ tests/test_TC_015_01.py
@@ -29,7 +29,9 @@ def test_login_form():
     driver.find_element(By.XPATH, "//input[@name='login-button']").click()
     time.sleep(1)
 
-    assert (driver.current_url == "https://www.saucedemo.com/inventory.html"), "We reached another site!"
+    assert (
+        driver.current_url == "https://www.saucedemo.com/inventory.html"
+    ), "We reached another site!"
 
 
 # Steps of test_ts015_01 | Save cart data of current user after re-login
@@ -44,9 +46,11 @@ def test_tc015_01():
     driver.find_element(By.XPATH, "//input[@name='login-button']").click()
     time.sleep(1)
 
-    assert (driver.current_url == "https://www.saucedemo.com/inventory.html"), "We reached another site!"
+    assert (
+        driver.current_url == "https://www.saucedemo.com/inventory.html"
+    ), "We reached another site!"
 
-# 1. Add product "Test.allTheThings() T-Shirt (Red)" to cart on the page https://www.saucedemo.com/inventory.html
+    # 1. Add product "Test.allTheThings() T-Shirt (Red)" to cart on the page https://www.saucedemo.com/inventory.html
     driver.get("https://www.saucedemo.com/inventory.html")
 
     # css_loc = ".inventory_list .inventory_item_label #item_3_title_link"
@@ -57,24 +61,30 @@ def test_tc015_01():
     time.sleep(1)
 
     url_item = "https://www.saucedemo.com/inventory-item.html?id=3"
-    assert (driver.current_url == url_item), "Не зашли в карточку нужного товара"
+    assert (
+        driver.current_url == url_item
+    ), "Не зашли в карточку нужного товара"
 
     css_loc = ".inventory_details_desc_container button"
-    driver.find_element(By.CSS_SELECTOR, css_loc).click()   # ADD TO CARD
+    driver.find_element(By.CSS_SELECTOR, css_loc).click()  # ADD TO CARD
     time.sleep(1)
 
-# 2. Проверка что добавили товар в корзину
+    # 2. Проверка что добавили товар в корзину
     css_loc = ".shopping_cart_link"
-    driver.find_element(By.CSS_SELECTOR, css_loc).click()   # вошли в Корзину
+    driver.find_element(By.CSS_SELECTOR, css_loc).click()  # вошли в Корзину
     time.sleep(1)
-    assert (driver.current_url == "https://www.saucedemo.com/cart.html"), "Не зашли в корзину"
+    assert (
+        driver.current_url == "https://www.saucedemo.com/cart.html"
+    ), "Не зашли в корзину"
 
     css_loc = ".cart_list > .cart_item .inventory_item_name"
     item_name = driver.find_element(By.CSS_SELECTOR, css_loc).text
     time.sleep(1)
-    assert (item_name == "Test.allTheThings() T-Shirt (Red)"), "В корзине нет нужного товара"
+    assert (
+        item_name == "Test.allTheThings() T-Shirt (Red)"
+    ), "В корзине нет нужного товара"
 
-# 3. Сlick the hamburger menu button in the upper left corner of the header. Click "LOGOUT" from the dropdown list
+    # 3. Сlick the hamburger menu button in the upper left corner of the header. Click "LOGOUT" from the dropdown list
     css_loc = "#react-burger-menu-btn"
     driver.find_element(By.CSS_SELECTOR, css_loc).click()
     time.sleep(1)
@@ -83,22 +93,27 @@ def test_tc015_01():
     driver.find_element(By.CSS_SELECTOR, css_loc).click()
     time.sleep(1)
     # проверяем, что вышли из системы на головную страницу
-    assert (driver.current_url == "https://www.saucedemo.com/"), \
-        "Мы не вышли из магазина, на главную страницу авторизации"
+    assert (
+        driver.current_url == "https://www.saucedemo.com/"
+    ), "Мы не вышли из магазина, на главную страницу авторизации"
 
-# 4. On the page https://www.saucedemo.com/. Input the username: standard_user and password: secret_sauce
-# and press "Login" button
+    # 4. On the page https://www.saucedemo.com/. Input the username: standard_user and password: secret_sauce
+    # and press "Login" button
     test_login_form()
 
-# 5. Press "Cart" icon on the page "https://www.saucedemo.com/inventory.html"
+    # 5. Press "Cart" icon on the page "https://www.saucedemo.com/inventory.html"
     css_loc = ".shopping_cart_link"
-    driver.find_element(By.CSS_SELECTOR, css_loc).click()   # вошли в Корзину
+    driver.find_element(By.CSS_SELECTOR, css_loc).click()  # вошли в Корзину
     time.sleep(1)
-    assert (driver.current_url == "https://www.saucedemo.com/cart.html"), "Не зашли в корзину"
+    assert (
+        driver.current_url == "https://www.saucedemo.com/cart.html"
+    ), "Не зашли в корзину"
 
     css_loc = ".cart_list > .cart_item .inventory_item_name"
     item_name = driver.find_element(By.CSS_SELECTOR, css_loc).text
-    assert (item_name == "Test.allTheThings() T-Shirt (Red)"), "В корзине нет нужного товара"
+    assert (
+        item_name == "Test.allTheThings() T-Shirt (Red)"
+    ), "В корзине нет нужного товара"
 
 
 print("\nquit browser...")
