db0afdbbbd2f5f4871259698b8ed736b2c8c6cd0
Oleg <114308068+OlegKomissarovV@users.noreply.github.com>
2022-11-22 00:03:31 +0300

    OK update conftest with hook install pytest html (#10)
    * Update conftest with hook install pytest html
    
    * Update action.yml

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 import conf
+import os
 from selenium import webdriver as WB
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -47,6 +49,37 @@ def g(d):
     print("\n*** end fixture = teardown ***\n")
 
 
-# Убрать комментарии после оформления html report
-# def pytest_html_report_title(report):
-#     report.title = "REPORT"
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    pytest_html = item.config.pluginmanager.getplugin("html")
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, "extra", [])
+    if report.when == "call":
+        feature_request = item.funcargs["request"]
+        driver = feature_request.getfixturevalue("d")
+        xfail = hasattr(report, "wasxfail")
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            report_dir = os.path.dirname(item.config.option.htmlpath)
+            len_dir = len(os.path.dirname(item.nodeid))
+            file_name = report.nodeid[len_dir:].replace("::", "_")[1:] + ".png"
+            destination_file = os.path.join(report_dir, file_name)
+
+            def S(X):
+                return driver.execute_script(
+                    "return document.body.parentNode.scroll" + X
+                )
+
+            driver.set_window_size(S("Width"), S("Height"))
+            driver.find_element(By.TAG_NAME, "body").screenshot(destination_file)
+            if file_name:
+                html = (
+                    '<div><img src="%s" alt="screenshot" style="width:300px;height:200px" onclick="window.open('
+                    'this.src)" align="right"/></div>' % file_name
+                )
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+
+def pytest_html_report_title(report):
+    report.title = "REPORT"
