bc8561483a61a3ad6462bbde100ef45498d57bae
”Natalia <n.i.tsyrelchuk@gmail.com>
2022-11-24 19:15:53 +0500

    some new locators and methods in cart_page

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -27,3 +27,17 @@ class CartPage(BasePage):
     def check_footer(self):
         assert self.element_is_present(*PageLocators.ROBOT), "something went wrong"
         self.browser.find_element(*PageLocators.PRIVACY).click()
+
+    def check_the_quantity(self):
+        quant = self.browser.find_element(*CartPageLocators.QUANTITY).text
+        if quant != 0:
+            assert "there are items in the cart", "the cart is empty"
+
+    # Opens hamburger menu
+    def open_hamburger(self):
+        self.browser.find_element(*PageLocators.HAMBURGER).click()
+
+    # Top left menu "all items" line
+    def click_all_items(self):
+        self.element_is_visible(PageLocators.ALL_ITEMS)
+        self.browser.find_element(*PageLocators.ALL_ITEMS).click()
