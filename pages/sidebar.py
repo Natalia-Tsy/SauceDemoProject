c83726092fd6d418ab30e9b9f6544df0e8197a2a
Alexander Tomelo <qa.tomelo.an@gmail.com>
2022-12-14 14:01:56 +0300

    correction sidebar.py file

--- pages/sidebar.py
+++ pages/sidebar.py
@@ -3,13 +3,18 @@ from .locators import SideBarLocator
 
 
 class SideBar(BasePage):
+    """This class is used to test the functionality of the sidebar on products page.
 
-    # Opens hamburger menu
+    Args:
+        BasePage: Contains common methods that are used by all page classes
+    """
+
     def click_hamburger_menu(self):
+        """Check that hamburger menu on the current page is visible and open it."""
         self.element_is_visible(SideBarLocator.HAMBURGER)
         self.browser.find_element(*SideBarLocator.HAMBURGER).click()
 
-    # Select and click LOGOUT item from hamburger menu
     def click_logout(self):
+        """Check that LOGOUT item is visible in hamburger menu and click it."""
         self.element_is_visible(SideBarLocator.LOGOUT)
         self.browser.find_element(*SideBarLocator.LOGOUT).click()
