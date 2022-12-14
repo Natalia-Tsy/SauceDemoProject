dfc41dff475b0d3160228c75f8a07f4035576074
Oleg <114308068+OlegKomissarovV@users.noreply.github.com>
2022-12-14 14:09:53 +0300

    Docs sidebar (#30)
    added docstring

--- pages/sidebar.py
+++ pages/sidebar.py
@@ -3,13 +3,18 @@ from .locators import SideBarLocator
 
 
 class SideBar(BasePage):
+    """This class is used to test the functionality of the side bar of the website.
 
-    # Opens hamburger menu
+    Args:
+        BasePage: Contains common methods that are used by all page classes
+    """
+
     def click_hamburger_menu(self):
+        """Open the hamburger menu, allowing the user to access the options it contains."""
         self.element_is_visible(SideBarLocator.HAMBURGER)
         self.browser.find_element(*SideBarLocator.HAMBURGER).click()
 
-    # Select and click LOGOUT item from hamburger menu
     def click_logout(self):
+        """Click the logout button in the side bar of the page."""
         self.element_is_visible(SideBarLocator.LOGOUT)
         self.browser.find_element(*SideBarLocator.LOGOUT).click()
