abc70098b6b0865f0368a4c14f2876ccedd2fe9f
AbaevMariya <41700081+AbaevMariya@users.noreply.github.com>
2022-12-16 11:39:11 +0200

    Update tests TC_005_01, TC_007_01, TC_007_03 (#33)
    * Update tests TC_005_01, TC_007_01, TC_007_03

--- tests/test_TC_005_01_view_details_for_product.py
+++ tests/test_TC_005_01_view_details_for_product.py
@@ -1,12 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/11/15 16:55
+# @Author  : Mariya Abayev
+
 import pytest
+import allure
 from pages.login_page import LoginPage
 from pages.product_page import ProductPage
 from pages.products_page import ProductsPage
-
-# view_details_for_product
+from src.src import LoginPageSrc, ProductsPageSrc, ProductPageSrc
 
 
 class Tests:
+    @allure.feature("US_005 | View product page")
+    @allure.story("TC_005.01 | View details for product from products page")
     @pytest.mark.parametrize(
         "username, password",
         [
@@ -24,18 +30,15 @@ class Tests:
             ),
         ],
     )
-    # view details
     def test_TC_005_01_view_details_for_product(self, d, username, password):
-        link = "https://www.saucedemo.com/"
-        page = LoginPage(d, link)
+        page = LoginPage(d, LoginPageSrc.URL)
         page.open_page()
         page.register_user(username, password)
-        link = "https://www.saucedemo.com/inventory.html"
-        page = ProductsPage(d, link)
+        page = ProductsPage(d, ProductsPageSrc.URL)
         page.should_be_products_page()
         item = page.select_product()
         page.go_to_product_page()
-        page = ProductPage(d, link)
+        page = ProductPage(d, ProductPageSrc.LINK)
         page.should_be_product_page()
         page.check_product_img(item)
         page.check_product_name(item)
